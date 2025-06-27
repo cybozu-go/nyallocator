837ff7c4e66897e387f3c6c457dd42362ce31b8b
YZ775 <yuzuki-mimura@cybozu.co.jp>
2025-06-27 06:30:05 +0000

    fix test

--- internal/controller/nodetemplate_controller_test.go
+++ internal/controller/nodetemplate_controller_test.go
@@ -12,6 +12,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/workqueue"
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,8 +38,9 @@ var _ = Describe("NodeTemplate Controller", func() {
 			})
 			Expect(err).ToNot(HaveOccurred())
 			nodeTemplateReconciler := &NodeTemplateReconciler{
-				Client: mgr.GetClient(),
-				Scheme: scheme,
+				Client:             mgr.GetClient(),
+				Scheme:             scheme,
+				RequeueRateLimiter: workqueue.NewTypedItemExponentialFailureRateLimiter[ctrl.Request](1*time.Second, 1*time.Second),
 			}
 			Expect(nodeTemplateReconciler.SetupWithManager(mgr)).To(Succeed())
 			nodeReconciler := &NodeReconciler{
